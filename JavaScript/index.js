de305c2492bbf1f7aa845ede7dfb133e2ec2ed0d
Posta Lajos <postalalijw@gmail.com>
2024-12-19 12:40:10 +0100

    index.js, header.css index.html móddosítva

---
+++ JavaScript/index.js
@@ -0,0 +1,16 @@
+const navList = document.querySelector(".nav-list-container");
+const menuItem = document.querySelectorAll(".menuItem");
+const hamburger = document.querySelector(".hamburger-button");
+
+
+function toggleMenu() {
+    if (nav-list-container.c) {
+
+    }
+}
+
+hamburgerbutton.addEventListener("click", toogleMenu);
+
+https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
+
+Easy hamburger menu with JS - Beginners dev.to
