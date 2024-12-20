69d444b67cf51de3e3e8e9a8c8a25f65a9e5df84
Posta Lajos <postalalijw@gmail.com>
2024-12-20 12:32:06 +0100

    hamburger es  navlistcontainer constansok, hamburger esemenyfigyelo es function menuOpen implementálása

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,16 +1,8 @@
-/*const navList = document.querySelector(".nav-list-container");
-const menuItem = document.querySelectorAll(".menuItem");
 const hamburger = document.querySelector(".hamburger-button");
+const navlistcontainer = document.querySelector(".nav-list-container");
 
+hamburger.addEventListener("click", menuOpen);
 
-function toggleMenu() {
-    if (nav-list-container.c) {
-
-    }
+function menuOpen() {
+    navlistcontainer.classList.toggle("active");
 }
-
-hamburgerbutton.addEventListener("click", toogleMenu);
-
-https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0
-
-Easy hamburger menu with JS - Beginners dev.to*/
