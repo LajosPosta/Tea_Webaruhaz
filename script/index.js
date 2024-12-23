694890b3989a27f1c21d1db9aeeb4e62ab6faf8b
Posta Lajos <postalalijw@gmail.com>
2024-12-23 11:55:46 +0100

    index.html, index.js, config.css módosítva

--- script/index.js
+++ script/index.js
@@ -3,10 +3,17 @@
 const hamburgerBtn = document.querySelector(".hamburger-button");
 const navListContainer = document.querySelector(".nav-list-container");
 const closeBtn = document.querySelector(".close-icon")
+const blacklay = document.querySelector(".mobileUnderlay")
 
 hamburgerBtn.addEventListener("click", menuOpen);
 closeBtn.addEventListener("click", menuOpen);
 
 function menuOpen() {
     navListContainer.classList.toggle("active");
+    /*underLay();*/
+    
 }
+
+/*function underLay() {
+    blacklay.classList.toggle("abc");
+}*/
