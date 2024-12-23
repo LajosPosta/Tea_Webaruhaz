353776a2ea665d0673ad66b0f0c8edd5ac0ba10e
Posta Lajos <postalalijw@gmail.com>
2024-12-23 16:23:57 +0100

    close ikon átszínezve, overlay hozzáadva

--- script/index.js
+++ script/index.js
@@ -3,17 +3,15 @@
 const hamburgerBtn = document.querySelector(".hamburger-button");
 const navListContainer = document.querySelector(".nav-list-container");
 const closeBtn = document.querySelector(".close-icon")
-const blacklay = document.querySelector(".mobileUnderlay")
+const overlay = document.querySelector(".overlay")
+
 
 hamburgerBtn.addEventListener("click", menuOpen);
 closeBtn.addEventListener("click", menuOpen);
+overlay.addEventListener("click", menuOpen)
 
 function menuOpen() {
     navListContainer.classList.toggle("active");
-    /*underLay();*/
-    
+    /*overlay.classList.toggle("activee");*/
+   
 }
-
-/*function underLay() {
-    blacklay.classList.toggle("abc");
-}*/
