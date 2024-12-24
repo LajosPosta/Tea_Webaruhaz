21c39ab9e4faa6cd068d329172bdc9f60dc61ad6
Posta Lajos <postalalijw@gmail.com>
2024-12-24 08:35:40 +0100

    nav-list-container módosítása

--- script/index.js
+++ script/index.js
@@ -4,14 +4,18 @@ const hamburgerBtn = document.querySelector(".hamburger-button");
 const navListContainer = document.querySelector(".nav-list-container");
 const closeBtn = document.querySelector(".close-icon")
 const overlay = document.querySelector(".overlay")
-
+const products = document.querySelector(".nav-item-products")
 
 hamburgerBtn.addEventListener("click", menuOpen);
 closeBtn.addEventListener("click", menuOpen);
-overlay.addEventListener("click", menuOpen)
+overlay.addEventListener("click", menuOpen);
+products.addEventListener("click", productsOpen)
 
 function menuOpen() {
     navListContainer.classList.toggle("active");
-    /*overlay.classList.toggle("activee");*/
    
+}
+
+function productsOpen() {
+    products.classList.toggle("products-active");
 }
