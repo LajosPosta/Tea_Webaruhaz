66a68e91e155c0eb0038db306a32e223537a740c
Posta Lajos <postalalijw@gmail.com>
2024-12-26 10:11:26 +0100

    carousel slidePosition függveny és rightButton esemenyfigyelo implementálva

--- script/index.js
+++ script/index.js
@@ -1,4 +1,4 @@
-/* Nav bar aktíválása*/
+/* Nav BAR*/
 
 const hamburgerBtn = document.querySelector(".hamburger-button");
 const navListContainer = document.querySelector(".nav-list-container");
@@ -16,11 +16,6 @@ overlay.addEventListener("click", menuOpen);
 navItemContainer.addEventListener("click", productsOpen);
 arrow.addEventListener("click", productsOpen);
 
-/*hamburgerBtn.addEventListener("click", productsClose);
-closeBtn.addEventListener("click", productsClose);
-overlay.addEventListener("click", productsClose);
-navItemContainer.addEventListener("click", productsClose);
-arrow.addEventListener("click", productsClose);*/
 
 function menuOpen() {
     navListContainer.classList.toggle("nav-list-container-active");   
@@ -31,5 +26,45 @@ function productsOpen() {
     arrow.classList.toggle("products-arrow_is-active")
     products.classList.toggle("products-is-active")
 }
+
+/***** CAROUSEL *****/
+
+const track = document.querySelector(".carousel_track");
+const slides = Array.from(track.children);
+const rightButton = document.querySelector(".carousel_button-right");
+const leftButton = document.querySelector(".carousel_button-left");
+const indicatorsContainer = document.querySelector(".carousel_indicator-container");
+const indicators = Array.from(indicatorsContainer.children)
+
+// a viewport szélességéhez rendezi a dia szélességét
+const slideWidth = slides[0].getBoundingClientRect().width; 
+
+// console.log(slideWidth);
+
+/* diák egymáshoz helyezése */
+// egy diának beállítjuk a pozícióját a setSlidePosition fügvénnyel
+const setSlidePosition = (slide, index) => {
+    slide.style.left = slideWidth * index + "px";
+};
+//majd az összes diára futtajuk (meghívjuk) a setSlidePosition függvényt
+slides.forEach(setSlidePosition);
+
+// Amikor balra klikkelek balra lévő diára váltok
+
+
+// Amikor jobbra klikkelek jobb lévő diára váltok
+rightButton.addEventListener("click", () => {
+const currentSlide = track.querySelector(".current-slide");
+const nextSlide = currentSlide.nextElementSibling;
+const amountToMove = nextSlide.style.left;
+
+console.log(amountToMove)
+})
+
+// A mikor a Nav indcatorra klikkelek akkor az adott diára váltok
+
+
+
+
 
 
