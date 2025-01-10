3844d9c2a820ad7356a1efbfc35adb4d914fd456
Posta Lajos <postalalijw@gmail.com>
2025-01-10 08:31:41 +0100

    webp képek importálva, inndex.html módosítva, product.js kommentek módosítva, product.html és product.css módosítva

--- js/product.js
+++ js/product.js
@@ -1,30 +1,34 @@
-//kiválasztom a slide elemet
+/***** Termék oldal *****/
+
+
+//kiválasztom a product-slider osztályú elemet
 const poductImageSlide = document.querySelector(".product-slider");
-//kiválasztom az összes képet product-images-container-ben
-const productImages = document.querySelectorAll(".product-images-container img");
+//kiválasztom az összes "Kamilla" osztályú képet
+const productImages = document.querySelectorAll(".Kamilla");
 
 //kezdeti értéket adok a kezdő képnek azaz a NULLADIK indexálású képpel kezdünk
 let activeImageSlide = 0;
 
-//mindegyik képre (forEach) alkalmazom az alábbiakat,
+//mindegyik képre (forEach metódust) alkalmazom az alábbiakat,
 // a nyílfüggvényben veszem az összes képet és veszem azok indexeit
-productImages.forEach((item, i) => {
+productImages.forEach((image, i) => {
         // klikk eseményt rendelek minden képhez
-        item.addEventListener("click", () => {
+        image.addEventListener("click", () => {
         // az aktív képről leveszem az active osztályt így az már nem aktív  
         productImages[activeImageSlide].classList.remove('active');
         // hozzáadam az active osztályt az újonnan kiválasztott képhez
-        item.classList.add("active");
-        // ezzel egyidejűleg megváltozik a product-slide osztályú html elem háttérképe is
-        poductImageSlide.style.backgroundImage = `url('${item.src}')`;
-        // az activeImageSlide változó felveszi az aktív kép indexét.
+        image.classList.add("active");
+        // a product-slider osztályú html tag forrása = az aaktuális kép forrásával 
+        poductImageSlide.src = image.src;
+        // az activeImageSlide változó felveszi az aktuális kép indexét.
         activeImageSlide = i;
 
     })
 })
 
-// kiszerelés gomboknak funkciót adunk
+/***** Kiszerelés gombok  *****/
 
+// kiszerelés gomboknak funkciót adunk
 const weightBtn = document.querySelectorAll(".weight-btn");
 // az aktuálisan kiválasztott gomb indexe
 let checkedBtn = 0;
